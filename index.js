de58dd89185347bdfb1b8a17f789b5fdd337e195
Alejo Acosta <alejoacos@gmail.com>
2020-12-16 22:46:41 +0000

    Express Serve Static Files

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
+const morgan = require('morgan');
 const app = express();
-app.use((req, res, next) => {
-    console.log(req.headers);
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {    
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an express server</h1></body></html>');
@@ -13,4 +17,4 @@ app.use((req, res, next) => {
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-})
+});
